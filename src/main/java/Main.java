be6c170e64116ff2025da32d5eb7454d4fb3d423
Filipe <filipeh291@gmail.com>
2023-09-24 13:22:21 +0100

    Multiple PING support

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
 
-        //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
-        Socket clientSocket = null;
+        Socket clientSocket;
         int port = 6379;
         String pong = "+PONG\r\n";
+
         try {
             serverSocket = new ServerSocket(port);
             serverSocket.setReuseAddress(true);
-            // Wait for connection from client.
-            clientSocket = serverSocket.accept();
-            clientSocket.getOutputStream().write(pong.getBytes());
+
+            while (true) {
+                // Wait for connection from client.
+                clientSocket = serverSocket.accept();
+
+                // Handle multiple PING commands from the same connection.
+                handlePingCommands(clientSocket, pong);
+
+                // Close the client socket after handling the PING commands.
+                clientSocket.close();
+            }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
             try {
-                if (clientSocket != null) {
-                    clientSocket.close();
+                if (serverSocket != null) {
+                    serverSocket.close();
                 }
             } catch (IOException e) {
                 System.out.println("IOException: " + e.getMessage());
+            }
+        }
+    }
+
+    private static void handlePingCommands(Socket clientSocket, String pong) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        OutputStream outputStream = clientSocket.getOutputStream();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.equals("PING")) {
+                outputStream.write(pong.getBytes());
+                outputStream.flush();
             }
         }
     }
